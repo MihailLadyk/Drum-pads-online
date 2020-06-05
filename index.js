6f8f12a01b12de1a164ab0de9de7190695197c5f
MihailLadyk <lp346426201@gmail.com>
2020-06-05 12:10:35 +0300

    bg

--- index.js
+++ index.js
@@ -1,74 +1,6 @@
 "use strict"
 
 
-// // узнать номер клавиши____________________________
-// function keyPress(e) {
-//     let keyNum;
-//     if (window.event) {
-//         keyNum = window.event.keyCode;
-//     }
-//     else if (e) {
-//         keyNum = e.which;
-//     }
-//     console.log(keyNum);
-// }
-// document.onkeydown = keyPress;
-// // ________________________________
-
-// // добавить функцию на клавишу_______________
-// document.onkeydown = function(e){
-//     switch (e.keyCode) {
-//         case 49: //номер клавиши
-//             console.log('Нажата единица');//функция
-//             break;//остановка
-//         case 50:
-//             console.log('Нажата двойка');
-
-//         default:
-//             console.log(e.keyCode);
-//     }
-// };
-// // __________________
-
-
-
-// document.body.onload = function(){
-// document.getElementById('button1').onclick = function(){
-//     console.log('button 1 click');
-// }
-//     document.getElementById('button2').onclick = function(){
-//         console.log('button 2 click');
-//         //функция для кнопки
-//     }
-//     document.getElementById('button3').onclick = function(){
-//         console.log('button 3 click');
-//         //функция для кнопки
-//     }
-//     document.getElementById('button4').onclick = function(){
-//         console.log('button 4 click');
-//         //функция для кнопки
-//     }
-//     document.getElementById('button5').onclick = function(){
-//         console.log('button 5 click');
-//         //функция для кнопки
-//     }
-//     document.getElementById('button6').onclick = function(){
-//         console.log('button 6 click');
-//         //функция для кнопки
-//     }
-//     document.getElementById('button7').onclick = function(){
-//         console.log('button 7 click');
-//         //функция для кнопки
-//     }
-//     document.getElementById('button8').onclick = function(){
-//         console.log('button 8 click');
-//         //функция для кнопки
-//     }
-//     document.getElementById('button9').onclick = function(){
-//         console.log('button 9 click');
-//         //функция для кнопки
-//     }
-// }
 
 
 
@@ -76,23 +8,27 @@
 
 
 
-
-
-
-
-
+let wrapper = document.querySelector('.wrapper');
 let  modal = document.querySelector('.modal');
-let themeChanger = document.querySelector('.theme__changer')
+let themeChanger = document.querySelector('.theme__changer');
+
 let blue = document.querySelector('.blue');
 let  green = document.querySelector('.green');
 let  gray = document.querySelector('.gray');
 let  red = document.querySelector('.red');
-let wrapper = document.querySelector('.wrapper')
-let  body = document.querySelector('body')
-let inp = document.querySelector('input')
-let otherLaunchPad = document.querySelector('#other')
-let drumsLaunchPad = document.querySelector('#drums')
-let guitarsLaunchPad = document.querySelector('#guitar')
+
+
+let colorInp = document.querySelector('.color__inp');
+let  body = document.querySelector('body');
+let inp = document.querySelector('#inp__volume');
+let bgInp = document.querySelector('.bg__inp');
+
+let otherLaunchPad = document.querySelector('#other');
+let drumsLaunchPad = document.querySelector('#drums');
+let guitarsLaunchPad = document.querySelector('#guitar');
+
+
+
 
 let modalFunc = function() {
 wrapper.style.display = 'block'
@@ -271,12 +207,8 @@ let inpFunc = function() {
     audio27.volume = inp.value / 100
    
 }
-
-
-
-
 inp.addEventListener('change', inpFunc)
 
 
 
-
+colorInp.addEventListener('input', () => body.style.backgroundColor = colorInp.value)
